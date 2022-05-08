9c0ae10ac3e2cee654746486a79a05c67b1d07b0
Wamaitha <wamaithaweru19@gmail.com>
2022-05-08 15:06:59 +0300

    add more template files

--- app/views.py
+++ app/views.py
@@ -8,15 +8,28 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    return render_template('index.html', title = "Home page")
 
 @app.route('/login')
 def login():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('login.html')
+    
+    return render_template('login.html', title = 'Login')
+
+
+@app.route('/category')
+def category():
+
+    
+    return render_template('category.html', title = 'Category')
+
+
+@app.route('/sign_up')
+def sign_up():
+
+    
+    return render_template('sign_up.html', title = 'Registration')
+
 
 
 
