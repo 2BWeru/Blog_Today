d92af0cf68b70a16d3792f0dbd33739b037d9645
Wamaitha <wamaithaweru19@gmail.com>
2022-05-14 02:38:07 +0300

    created mde configurations

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,39 +1,42 @@
-from crypt import methods
 from flask import flash, render_template,redirect,url_for,request
 from . import auth
 from flask_login import login_user,login_user,logout_user,login_required
-from .forms import RegistrationForm,LoginForm
+from .forms import LoginForm,RegisterForm
 from .. import db
+from ..email import mail_message
 from ..models import User
 
-@auth.route('/register', methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-
-    return render_template('auth/sign_up.html', title = "Create an  Account", form = form)
-
-
-@auth.route('/login' , methods=["GET","POST"])
+@auth.route('/login' , methods = ["GET","POST"])
 def login():
      login_form = LoginForm()
      if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
+        user = User.query.filter_by(username = login_form.username.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(request.args.get('next') or url_for('main.all_pitches'))
 
         flash('Invalid username or Password')
 
 
-     return render_template('auth/login.html', login_form = login_form)
+     return render_template('auth/login.html', form = login_form)
     
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+@auth.route('/register', methods = ["GET","POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user.save_user()
+    
+        # mail_message("Welcome to Pitch_whorl","email/welcome_user",user.email,user=user)
+        
+        return redirect(url_for('main.account',uname=user))
+
+    return render_template('auth/sign_up.html', title = "Create an  Account", form = form)
+
+
--- config.py
+++ config.py
@@ -6,6 +6,14 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wamaitha:Wammy@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'Yellow'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+     
    
     
 
