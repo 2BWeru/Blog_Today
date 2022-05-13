46c6de66af1a73fff6d47bf5cd67c2b0e1ca6a67
Wamaitha <wamaithaweru19@gmail.com>
2022-05-14 02:33:56 +0300

    update login and registration forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,30 +4,36 @@ from wtforms.validators import InputRequired,Email,EqualTo,length
 from wtforms import ValidationError
 from ..models import User
 
-class RegistrationForm(FlaskForm):
+class LoginForm(FlaskForm):
+    username = StringField(validators={InputRequired(),length(min=4,
+         max=20)},render_kw={"placeholder":"username"})
+    
+    password = PasswordField('Password',validators = [InputRequired()]
+        ,render_kw={"placeholder":"Password"})
+
+    remember = BooleanField(render_kw={'pacehlder','Remember me'})
+
+    submit = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
     username = StringField(validators={InputRequired(),length(min=4,
          max=20)},render_kw={"placeholder":"username"})
 
     email = StringField(validators=[InputRequired(),Email()],render_kw={"placeholder":"email"})
 
-    password = PasswordField('Password',validators = [InputRequired(),
-        EqualTo('password_confirm',message = 'Passwords must match')],render_kw={"placeholder":"Password"})
-    
-    password_confirm = PasswordField(render_kw={'Confirm Passwords'},validators = [InputRequired()])
+    password = PasswordField(validators = {InputRequired()
+       },render_kw={"placeholder":"Password"})
+
 
     submit = SubmitField("Register")
 
     
-def validate_email(self,data_field):
+def validate_email(data_field):
     if User.query.filter_by(email =data_field.data).first():
         raise ValidationError('There is an account with that email')
 
-def validate_username(self,data_field):
+def validate_username(data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
 
-class LoginForm(FlaskForm):
-    email = StringField(render_kw={'Your Email Address'},validators=[InputRequired(),Email()])
-    password = PasswordField(render_kw={'Password'},validators =[InputRequired()])
-    remember = BooleanField(render_kw={'Remember me'})
-    submit = SubmitField('Sign In')
