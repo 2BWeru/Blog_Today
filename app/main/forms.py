434597998dc6253f02228c3ab9b0aa5db4aa05db
Wamaitha <wamaithaweru19@gmail.com>
2022-05-14 02:45:15 +0300

    creates pitch,comments and votes forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField,SubmitField,BooleanField
+from wtforms import TextAreaField,SubmitField,StringField,SelectField,BooleanField
 from wtforms.validators import InputRequired,Email,EqualTo
 from wtforms import ValidationError
 from ..models import User
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
+    bio = TextAreaField(render_kw={'placeholder':'Tell us about you.'},validators = [InputRequired()])
+    submit = SubmitField('Submit')
+
+
+class PitchForm(FlaskForm):
+    title = StringField(validators=[InputRequired()],render_kw={"placeholder":"Title"})
+    category = SelectField(choices=[('Music','Music'),('Job','Job'),('Love','Love')],validators=[InputRequired()])
+    post = TextAreaField(validators=[InputRequired()],render_kw={"placeholder":"Your pitch"})
+    submit = SubmitField('Pitch')
+   
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment',validators=[InputRequired()],render_kw={"placeholder":"Comment"})
+    submit = SubmitField('Comment')
+
+#.....
+class ReviewForm(FlaskForm):
+    title = StringField('Review title',validators=[InputRequired()], render_kw={"placeholder":"title"})
+    review = TextAreaField('Movie review')
     submit = SubmitField('Submit')
