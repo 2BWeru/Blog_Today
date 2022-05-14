d9059a6700bc71bc61fd0518e87a97d140e7a875
Wamaitha <wamaithaweru19@gmail.com>
2022-05-14 14:17:31 +0300

    change app configuration to production class

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User
 
 
 # creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('runserver',Server)
