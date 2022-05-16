d18853d8b3698dfa1ad5d7f8b88b549f67c7a36a
Wamaitha <wamaithaweru19@gmail.com>
2022-05-17 00:50:02 +0300

    create Pitch,Comment,Upvote,Downvote  tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,17 +1,22 @@
+from unicodedata import category
 import unittest
-from app.models import User
+from app.models import User,Pitch,Comment,Upvote,Downvote
 
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = User(password = 'yellow')
+        self.new_pitch = Pitch(id='1',title='Test-title',pitch='test-post',category='test-category', pitch_id=1,user_id=1)
+        self.new_user = User(id='1',username='Luar',email='luar@gmail.com',password='luar34',bio='I love music')
+        self.new_comment = Comment(text='Awesome idea',pitch_id=1,user_id=1)
+        self.new_upvote = Upvote(pitch_id=1,author='Luar',user_id=1)
+        self.new_downvote = Downvote(pitch_id=1,author='Luar',user_id=1)
+    
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch, Pitch))
+        self.assertTrue(isinstance(self.new_user, User))
+        self.assertTrue(isinstance(self.new_comment, Comment))
+        self.assertTrue(isinstance(self.new_upvote, Upvote))
+        self.assertTrue(isinstance(self.new_downvote, Downvote))
 
-    def test_password_setter(self):
-        self.assertTrue(self.new_user.pass_secure is not None)
 
-    def test_no_access_password(self):
-            with self.assertRaises(AttributeError):
-                self.new_user.password
-
-    def test_password_verification(self):
-            self.assertTrue(self.new_user.verify_password('yellow'))
