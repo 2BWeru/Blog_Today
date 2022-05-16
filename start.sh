e4cbe8a1a0666a362a36864d8dd980875dad3874
Wamaitha <wamaithaweru19@gmail.com>
2022-05-16 21:09:10 +0300

    add exports

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+export SECRET_KEY = 'Yellow'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wamaitha:Wammy@localhost/pitch'
 export MAIL_USERNAME='pitcher24@gmail.com'
 export MAIL_PASSWORD='1234567'
 
