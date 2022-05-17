4f088186941757284977e01ddd169e3aaa8ecdba
Wamaitha <wamaithaweru19@gmail.com>
2022-05-17 10:28:51 +0300

    fix Database connection to class ProdConfig and DevConfig

--- config.py
+++ config.py
@@ -2,22 +2,27 @@ import os
 
 
 class Config:
-   
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
+
     SECRET_KEY = 'Yellow'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("EMAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
     
 
 class ProdConfig(Config):
-   
+    uri = os.getenv('DATABASE_URL')
+    if uri and uri.startswith('postgres://'):
+        uri = uri.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = uri
     pass
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wamaitha:Wammy@localhost/pitch'
     DEBUG = True
     
 
