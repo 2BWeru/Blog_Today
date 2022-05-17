819a6012eae2edee3537b33840939119e77e62e1
Wamaitha <wamaithaweru19@gmail.com>
2022-05-17 10:44:51 +0300

    fix Database URI in start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export SECRET_KEY = 'Yellow'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wamaitha:Wammy@localhost/pitch'
+export SECRET_KEY='Yellow'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://wamaitha:Wammy@localhost/pitch'
 export DATABASE_URL='postgres://tfkwsiruaqnbxu:2b54036cf2d10d0cb4ae4a836970ba1e204c5c311d8f63826ff458e28fbf5997@ec2-34-201-95-176.compute-1.amazonaws.com:5432/d8r04srep9p0d1'
 export MAIL_USERNAME='pitcher24@gmail.com'
 export MAIL_PASSWORD='1234567'
