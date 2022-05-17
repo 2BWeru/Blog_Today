b9899d8ebdbcd7c0add5a63474d179638fb69b85
Wamaitha <wamaithaweru19@gmail.com>
2022-05-17 12:56:12 +0300

    edit SQLALCHEMY Database URI

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
+export SECRET_KEY='Yellow'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://wamaitha:Wammy@localhost/pitch'
+export MAIL_USERNAME='pitcher24@gmail.com'
+export MAIL_PASSWORD='1234567'
+
 python3.8 manage.py server
