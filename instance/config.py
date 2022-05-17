f2899709480d0bddb1145db9a7cb57a91dd3c12b
Wamaitha <wamaithaweru19@gmail.com>
2022-05-17 11:45:25 +0300

    fix Database connection to class config

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,5 @@
 SECRET_KEY='Yellow'
-DATABASE_URI='postgresql+psycopg2://wamaitha:Wammy@localhost/pitch'
+DATABASE_URL='postgresql+psycopg2://wamaitha:Wammy@localhost/pitch'
 MAIL_USERNAME='pitcher24@gmail.com'
 MAIL_PASSWORD='1234567'
 
