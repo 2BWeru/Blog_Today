d09a80916f28b35639346f088bbab13706077e0f
Wamaitha <wamaithaweru19@gmail.com>
2022-05-19 07:44:51 +0300

    adds quotes url

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 class Config:
 
     SECRET_KEY = 'Yellow'
+    QUOTE_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     MAIL_SERVER = 'smtp.googlemail.com'
